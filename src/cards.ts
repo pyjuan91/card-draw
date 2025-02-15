6bab5ccfb2f1f7051061baa81782eb5c0460da48
Po-Yu Juan <pyjuan91@gmail.com>
2025-02-16 03:50:35 +0800

    [fix] cards image broken

--- src/cards.ts
+++ src/cards.ts
@@ -1,32 +1,34 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 export const cards = [
   {
     id: 1,
     name: "Chaewon 1",
-    image: "/images/chaewon_1.jpg",
+    image: `${BASE_URL}images/chaewon_1.jpg`,
   },
   {
     id: 2,
     name: "Chaewon 2",
-    image: "/images/chaewon_2.jpg",
+    image: `${BASE_URL}images/chaewon_2.jpg`,
   },
   {
     id: 3,
     name: "Chaewon 3",
-    image: "/images/chaewon_3.jpg",
+    image: `${BASE_URL}images/chaewon_3.jpg`,
   },
   {
     id: 4,
     name: "Kazuha 1",
-    image: "/images/kazuha_1.jpg",
+    image: `${BASE_URL}images/kazuha_1.jpg`,
   },
   {
     id: 5,
     name: "Kazuha 2",
-    image: "/images/kazuha_2.jpg",
+    image: `${BASE_URL}images/kazuha_2.jpg`,
   },
   {
     id: 6,
     name: "Kazuha 3",
-    image: "/images/kazuha_3.jpg",
+    image: `${BASE_URL}images/kazuha_3.jpg`,
   },
 ];
