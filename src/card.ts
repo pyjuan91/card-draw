b187a62ab904ae969b1bc7a26eea6d0028974d95
Po-Yu Juan <pyjuan91@gmail.com>
2025-02-16 02:57:00 +0800

    [fix] cards name

--- src/card.ts
+++ src/card.ts
@@ -1,32 +1,32 @@
 export const cards = [
   {
     id: 1,
-    name: "Idol 1",
+    name: "Chaewon 1",
     image: "/images/chaewon_1.jpg",
   },
   {
     id: 2,
-    name: "Idol 2",
+    name: "Chaewon 2",
     image: "/images/chaewon_2.jpg",
   },
   {
     id: 3,
-    name: "Idol 3",
+    name: "Chaewon 3",
     image: "/images/chaewon_3.jpg",
   },
   {
     id: 4,
-    name: "Idol 4",
+    name: "Kazuha 1",
     image: "/images/kazuha_1.jpg",
   },
   {
     id: 5,
-    name: "Idol 5",
+    name: "Kazuha 2",
     image: "/images/kazuha_2.jpg",
   },
   {
     id: 6,
-    name: "Idol 6",
+    name: "Kazuha 3",
     image: "/images/kazuha_3.jpg",
   },
 ];
