12bb740863b0f61f9ec2b791d8184add4b2b7aec
Po-Yu Juan <pyjuan91@gmail.com>
2025-02-16 02:44:49 +0800

    [update] add some cards and try deploying

---
+++ src/card.ts
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    id: 1,
+    name: "Idol 1",
+    image: "/images/chaewon_1.jpg",
+  },
+  {
+    id: 2,
+    name: "Idol 2",
+    image: "/images/chaewon_2.jpg",
+  },
+  {
+    id: 3,
+    name: "Idol 3",
+    image: "/images/chaewon_3.jpg",
+  },
+  {
+    id: 4,
+    name: "Idol 4",
+    image: "/images/kazuha_1.jpg",
+  },
+  {
+    id: 5,
+    name: "Idol 5",
+    image: "/images/kazuha_2.jpg",
+  },
+  {
+    id: 6,
+    name: "Idol 6",
+    image: "/images/kazuha_3.jpg",
+  },
+];
